33f719a9cef54b1a8fdaf80d8d997d4f1b57c208
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-12-16 13:53:08 +0200

    adding some security middleware

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
 //external modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
-
 const AppError = require('./utils/appError');
 const globleError = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+app.use(helmet());
 // middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'To many request from this ip ,please tray in an hour'
+});
+app.use('/api', limiter);
+
+//Body parser reading data from req.body
+app.use(express.json({ limit: '10kb' }));
+
+//data sanitization
+app.use(mongoSanitize());
+
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.headers);
+//   next();
+// });
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
