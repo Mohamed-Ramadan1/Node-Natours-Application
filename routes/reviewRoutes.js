59daa1620fd6ccded341391c9f0f0e0ffee4cadb
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-12-22 22:04:32 +0200

    Refactor review routes and controller

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
 const reviewsController = require('./../controllers/reviewsController');
 const authController = require('./../controllers/authController');
 
 router
   .route('/')
   .get(reviewsController.getAllReviews)
-  .post(authController.protect, reviewsController.createReview);
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewsController.createReview
+  );
 
 module.exports = router;
